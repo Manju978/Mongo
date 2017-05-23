4dd716aefc746e33470844db730452e2713a7737
user <user@10.12.61.174>
2017-05-23 17:53:38 +0530

    Mongo with servlet-->working fine

---
+++ mongoweb/src/main/java/mongoweb/converter/PersonConverter.java
@@ -0,0 +1,38 @@
+package mongoweb.converter;
+
+import org.bson.types.ObjectId;
+
+import com.mongodb.BasicDBObjectBuilder;
+import com.mongodb.DBObject;
+
+import mongoweb.model.Person;
+
+public class PersonConverter 
+{
+	public static DBObject toDbObject(Person p)
+	{
+		BasicDBObjectBuilder builder=BasicDBObjectBuilder.start().append("name", p.getName()).append("country", p.getCountry());
+		
+		if(p.getId()!=null)
+		{
+			builder=builder.append("_id", new ObjectId(p.getId()));
+			
+		}
+		
+		
+		
+		return builder.get();
+		
+	}
+	public static Person toPerson(DBObject doc) 
+	{
+		Person p=new Person();
+		p.setName((String) doc.get("name"));
+		p.setCountry((String) doc.get("country"));
+		ObjectId id=(ObjectId) doc.get("_id");
+		p.setId(id.toString());
+		return p;
+		
+	}
+
+}
---
+++ mongoweb/src/main/java/mongoweb/model/Person.java
@@ -0,0 +1,28 @@
+package mongoweb.model;
+
+public class Person
+{
+	String id;
+	String name;
+	String country;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	
+
+}
